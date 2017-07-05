b0868726e56fc808c5945f425c3ebdc45bac0e5b
Arvin Arbuis <asarbuis@up.edu.ph>
2017-07-06 00:57:16 +0800

    Progress in adding attachments

--- routes-laboratories.js
+++ routes-laboratories.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Laboratory = require('./models').Laboratory;
+const multer = require('multer');
 
 ///////////////////// MIDDLEWARES ////////////////////////
 
@@ -21,18 +22,26 @@ function requireDoctor(req, res, next) {
 	next();
 }
 
+var upload = multer({ dest: './uploads/lab_results' })
+
 //////////////////////// GET ////////////////////////////////////
 
-router.get('/laboratory_list', function(req, res){
+router.get('/laboratory_list', function (req, res) {
 	
 });
 
-router.get('/laboratory_add', function(req, res){
-
+router.get('/laboratory_add', function (req, res) {
+	res.render('treatments/add-lab-results.html');
 });
 
 router.get('/laboratory_edit/:id', function(req, res){
 
+});
+
+router.post('/laboratory_add', upload.array('attachments[]'), function (req, res) {
+	console.log(req.body);
+	console.log(req.files);
+	res.redirect('/laboratory_add');
 });
 
 module.exports = router;
