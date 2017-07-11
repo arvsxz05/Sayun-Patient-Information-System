8d297d298ad83b91fd0a4b88a0a5f64aff2f30ae
Mae Celine Erasmo <mae.celinerasmo@gmail.com>
2017-07-11 10:58:58 +0800

    added edit ipt/opt

--- routes-inpatient-treatment.js
+++ routes-inpatient-treatment.js
@@ -54,66 +54,32 @@ router.get('/ipt_edit_json/:ipt_id/:patient_id', function (req, res) {
 	console.log(req.params);
 
 	var key = req.params.ipt_id;
-	var pkey = req.params.patient_id, result;
+	var patient_id = req.params.patient_id, result;
 	var doctors = [];
 
-	Doctor.findAll({
+	InPatient_Treatment.findOne({
+		raw: true,
 		include: [{
-	        model: User_Account,
-	        where: {
-				spisInstanceLicenseNo: req.session.spisinstance.license_no,
+			model: Check_Up,
+			where: {
+				patientId: patient_id,
 			},
-	        as: 'username',
-	    }],
-	    raw: true,
-    }).then(function(results){
-
-    	for(var i = 0; i < results.length; i++){
-    		result = results[i];
-
-    		doctors.push({
-    			id: result.id,
-    			last_name: result['username.last_name'],
-    			first_name: result['username.first_name'],
-    			middle_name: result['username.middle_name'],
-    		});
-    	}
-
-    	InPatient_Treatment.findOne({
-			raw: true,
+			as: 'parent_record',
 			include: [{
-		        model: Check_Up,
-		        where: {
-					patientId: pkey,
-				},
-				as: 'parent_record'
-		    }],
-		    where: {
-		    	id: key,
-		    }
-		}).then(function(result){
-
-			var ipt = {
-				id: result.id,
-				conf_date: result.conf_date,
-				discharge_date: result.discharge_date,
-				sum_of_diag: result.sum_of_diag,
-				detailed_diag: result.detailed_diag,
-				notes: result.notes,
-				attachments: result.attachments,
-				status: result.status,
-				doctorId: result['parent_record.doctorId'],
-				hospital: result['parent_record.hospitalName'],
-				check_upId: result['parent_record.id'],
+				model: Doctor,
+				include: [{
+					model: User_Account,
+					as: 'username'
+				}],
+			}],
+		}],
+		where: {
+				id: key,
 			}
+	}).then(function(result){
+		res.json({ipt: result});
+	})
 
-			res.json({
-				doctors: doctors,
-				ipt: ipt,
-				doctor: req.session.doctor,
-			})
-		});
-    });
 });
 
 /////////////////////////// POST ////////////////////////////////////
@@ -132,7 +98,7 @@ router.post('/ipt_add', function(req, res){
 	var doc = req.body['doctor'];
 	var discharge = null;
 
-	if(!req.body['discharge-date'] && req.body['discharge-date'].trim() !== "") {
+	if(req.body['discharge-date'] && req.body['discharge-date'].trim() !== "") {
 		discharge = req.body['discharge-date'];
 	};
 
@@ -162,26 +128,32 @@ router.post('/ipt_edit/:ipt_id/:cu_id', function(req, res){
 	var key = req.params.ipt_id;
 	var cu_id = req.params.cu_id;
 
-	console.log(key, cu_id);
+	// console.log(key, cu_id);
 
 	var confine = null, discharge = null;
 
-	if(req.body['date_']['year'][0] != '' && req.body['date_']['month'][0] != '' && req.body['date_']['day'][0] != ''){
-		confine = req.body["date_"]["year"][0]+"-"+req.body["date_"]["month"][0]+"-"+req.body["date_"]["day"][0];
+	// if(req.body['date_']['year'][0] != '' && req.body['date_']['month'][0] != '' && req.body['date_']['day'][0] != ''){
+	// 	confine = req.body["date_"]["year"][0]+"-"+req.body["date_"]["month"][0]+"-"+req.body["date_"]["day"][0];
+	// }
+
+	// if(req.body['date_']['year'][1] != '' && req.body['date_']['month'][1] != '' && req.body['date_']['day'][1] != ''){
+	// 	discharge = req.body["date_"]["year"][1]+"-"+req.body["date_"]["month"][1]+"-"+req.body["date_"]["day"][1];
+	// }
+
+	var dis_date = req.body['discharge-date'].split("-");
+	console.log(dis_date);
+	if((dis_date[0] != "" && dis_date[1] != "" && dis_date[2] != "") && (dis_date[0] != 'null' && dis_date[1] != 'null' && dis_date[2] != 'null') ){
+		discharge = req.body['discharge-date'];
+		console.log("DISCHARGE DATE");
 	}
 
-	if(req.body['date_']['year'][1] != '' && req.body['date_']['month'][1] != '' && req.body['date_']['day'][1] != ''){
-		discharge = req.body["date_"]["year"][1]+"-"+req.body["date_"]["month"][1]+"-"+req.body["date_"]["day"][1];
-	}
-
-	var hospital = req.body['edit-hospital'];
-	var summary = req.body['edit-summary'];
-	var details = req.body['edit-detailed-diagnosis'];
-	var notes = req.body['edit-notes'];
-	var doc = req.body['edit-doctor'];
-
-	var checkup_id;
-
+	confine = req.body['confinement-date'];
+	var hospital = req.body['hospital'];
+	var summary = req.body['summary'];
+	var details = req.body['detailed-diagnosis'];
+	var notes = req.body['notes'];
+	var doc = req.body['doctor'];
+	
 	InPatient_Treatment.update({
 		conf_date: confine,
 		discharge_date: discharge,
@@ -201,17 +173,16 @@ router.post('/ipt_edit/:ipt_id/:cu_id', function(req, res){
 			where: {
 				id: cu_id,
 			}
-		}).then(function(){
-			res.json({"status": "successfully updated!"});
-		}).then(function(error){
-			console.log("in here edit ipt");
+		}).then(function(result){
+			res.json({success: true});
+		}).catch(function(error){
 			console.log(error);
-			res.send({"status": "error"});
-		})
+			res.json({error: error});
+		});
 
 	}).catch(function(error){
 		console.log(error);
-		res.send({"status": "error"});
+		res.json({error: error});
 	});
 });
 
