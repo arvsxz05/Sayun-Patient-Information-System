f4baee280978c1655e085c2a2ba2103818c813bd
Mae Celine Erasmo <mae.celinerasmo@gmail.com>
2017-07-27 13:43:41 +0800

    billing

--- routes/routes-billing.js
+++ routes/routes-billing.js
@@ -7,6 +7,8 @@ const Billing_Item = require('../models/database').Billing_Item;
 const InPatient_Treatment = require('../models/database').InPatient_Treatment;
 const OutPatient_Treatment = require('../models/database').OutPatient_Treatment;
 const Consultation = require('../models/database').Consultation;
+const User_Account = require('../models/database').User_Account;
+const Patient = require('../models/database').Patient;
 const Sequelize = require('sequelize');
 
 ///////////////////// MIDDLEWARES ////////////////////////
@@ -188,184 +190,193 @@ router.get('/billing_list/:check_up_id', requireLoggedIn, function(req, res){
 });
 
 router.get('/financial_report', requireLoggedIn, function(req, res){
-	var key = ['16', '17', '18'];
-	Billing_Item.findAll({
-		raw: true,
-		attributes: [
-			[Sequelize.fn('SUM', Sequelize.col('amount')), 'sumOfColumn'],
-			'checkUpId',
-		],
-		group: ['billing_item.checkUpId'],
-		where: {
-			checkUpId: key,
-		}
-	}).then(billing_sum => {
-		console.log(billing_sum);
-		res.json({
-			billing_sum: billing_sum
-		})
-	});
+	// var key = ['16', '17', '18'];
+	var allRecords = [];
+	// Billing_Item.findAll({
+	// 	raw: true,
+	// 	attributes: [
+	// 		[Sequelize.fn('SUM', Sequelize.col('amount')), 'sumOfColumn'],
+	// 		'checkUpId',
+	// 	],
+	// 	group: ['billing_item.checkUpId'],
+	// 	where: {
+	// 		checkUpId: key,
+	// 	}
+	// }).then(billing_sum => {
+	// 	console.log(billing_sum);
+	// 	res.json({
+	// 		billing_totals: billing_sum
+	// 	});
+	// });
 
-	// if(req.session.doctor){
-	// 	InPatient_Treatment.findAll({
-	// 		raw: true,
-	// 		where: {
-	// 			active: true
-	// 		},
-	// 		attributes: ['id', 'conf_date'],
-	// 		group: ['inpatient_treatment.id', 'inpatient_treatment.conf_date', 'parent_record.id', 'parent_record.check_up_type', 'parent_record.hospitalName', 'parent_record.doctorId', 'parent_record.patientId', 'parent_record->billing_items.checkUpId'],
-	// 		include: [{
-	// 			model: Check_Up,
-	// 			as: 'parent_record',
-	// 			required: true,
-	// 			where: {
-	// 				active: true,
-	// 				doctorId: req.session.doctor.id,
-	// 			},
-	// 			attributes: ['id', 'check_up_type', 'hospitalName', 'doctorId', 'patientId' ],
-	// 			include: [{
-	// 				model: Billing_Item,
-	// 				as: 'billing_items',
-	// 				attributes: [
-	// 					[Sequelize.fn('SUM', Sequelize.col('amount')), 'sumOfColumn']
-	// 				],
-	// 			}]
-	// 		}],
-	// 	}).then(ipt_list => {
-	// 		console.log(ipt_list);
-	// 		OutPatient_Treatment.findAll({
-	// 			raw: true,
-	// 			where: {
-	// 				active: true,
-	// 			},
-	// 			attributes: ['id', 'date'],
-	// 			group: ['outpatient_treatment.id', 'outpatient_treatment.date', 'parent_record.id', 'parent_record.check_up_type', 'parent_record.hospitalName', 'parent_record.doctorId', 'parent_record.patientId', 'parent_record->billing_items.checkUpId'],
-	// 			include: [{
-	// 				model: Check_Up,
-	// 				as: 'parent_record',
-	// 				required: true,
-	// 				where: {
-	// 					active: true,
-	// 					doctorId: req.session.doctor.id,
-	// 				},
-	// 				attributes: ['id', 'check_up_type', 'hospitalName', 'doctorId', 'patientId' ],
-	// 				include: [{
-	// 					model: Billing_Item,
-	// 					as: 'billing_items',
-	// 					attributes: [
-	// 						[Sequelize.fn('SUM', Sequelize.col('amount')), 'sumOfColumn'],
-	// 					],
-	// 					group: ['billing_item.checkUpId']
-	// 				}]
-	// 			}]
-	// 		}).then(opt_list => {
-	// 			console.log(opt_list);
-	// 			Consultation.findAll({
-	// 				raw: true,
-	// 				attributes: ['id', 'date'],
-	// 				where: {
-	// 					active: true,
-	// 				},
-	// 				group: ['consultation.id', 'consultation.date', 'parent_record.id', 'parent_record.check_up_type', 'parent_record.hospitalName', 'parent_record.doctorId', 'parent_record.patientId', 'parent_record->billing_items.checkUpId'],
-	// 				include: [{
-	// 					model: Check_Up,
-	// 					as: 'parent_record',
-	// 					required: true,
-	// 					where: {
-	// 						active: true,
-	// 						doctorId: req.session.doctor.id,
-	// 					},
-	// 					attributes: ['id', 'check_up_type', 'hospitalName', 'doctorId', 'patientId' ],
-	// 					include: [{
-	// 						model: Billing_Item,
-	// 						as: 'billing_items',
-	// 						attributes: [
-	// 							[Sequelize.fn('SUM', Sequelize.col('amount')), 'sumOfColumn']
-	// 						],
-	// 						group: ['billing_item.checkUpId']
-	// 					}]
-	// 				}],
-	// 			}).then(cc_list =>{
-	// 				console.log(cc_list);
-	// 				// console.log(ipt_list.concat(opt_list.concat(cc_list)));
-	// 				res.json({
-	// 					meds: ipt_list.concat(opt_list.concat(cc_list)),
-	// 				});
-	// 			});
-	// 		});
-	// 	});
-	// } else{
-	// 	InPatient_Treatment.findAll({
-	// 		raw: true,
-	// 		where: {
-	// 			active: true
-	// 		},
-	// 		attributes: ['id', 'conf_date'],
-	// 		include: [{
-	// 			model: Check_Up,
-	// 			as: 'parent_record',
-	// 			required: true,
-	// 			where: {
-	// 				active: true,
-	// 			},
-	// 			attributes: ['id', 'check_up_type', 'hospitalName', 'doctorId', 'patientId' ],
-	// 			include: [{
-	// 				model: Billing_Item,
-	// 				as: 'billing_items',
-	// 				required: true,
-	// 			}]
-	// 		}],
-	// 	}).then(ipt_list => {
-	// 		OutPatient_Treatment.findAll({
-	// 			raw: true,
-	// 			where: {
-	// 				active: true,
-	// 			},
-	// 			attributes: ['id', 'date'],
-	// 			include: [{
-	// 				model: Check_Up,
-	// 				as: 'parent_record',
-	// 				required: true,
-	// 				where: {
-	// 					active: true,
-	// 				},
-	// 				attributes: ['id', 'check_up_type', 'hospitalName', 'doctorId', 'patientId' ],
-	// 				include: [{
-	// 					model: Billing_Item,
-	// 					as: 'billing_items',
-	// 					required: true,
-	// 				}]
-	// 			}]
-	// 		}).then(opt_list => {
-	// 			Consultation.findAll({
-	// 				raw: true,
-	// 				attributes: ['id', 'date'],
-	// 				where: {
-	// 					active: true,
-	// 				},
-	// 				include: [{
-	// 					model: Check_Up,
-	// 					as: 'parent_record',
-	// 					required: true,
-	// 					where: {
-	// 						active: true,
-	// 					},
-	// 					attributes: ['id', 'check_up_type', 'hospitalName', 'doctorId', 'patientId' ],
-	// 					include: [{
-	// 						model: Billing_Item,
-	// 						as: 'billing_items',
-	// 						required: true,
-	// 					}]
-	// 				}],
-	// 			}).then(cc_list =>{
-	// 				console.log(ipt_list.concat(opt_list.concat(cc_list)));
-	// 				res.json({
-	// 					meds: ipt_list.concat(opt_list.concat(cc_list)),
-	// 				});
-	// 			});
-	// 		});
-	// 	});
-	// }
+	if(req.session.doctor){
+		InPatient_Treatment.findAll({
+			where: {
+				active: true
+			},
+			attributes: ['id', ['conf_date', 'date']],
+			raw: true,
+			include: [{
+				model: Check_Up,
+				as: 'parent_record',
+				required: true,
+				where: {
+					active: true,
+					doctorId: req.session.doctor.id,
+				},
+				attributes: ['id', 'check_up_type', 'hospitalName', 'doctorId', 'patientId' ],
+				include: [{
+					model: Patient,
+					attributes: ['last_name', 'middle_name', 'first_name']
+				}]
+			}],
+		}).then(ipt_list => {
+			console.log(ipt_list);
+			OutPatient_Treatment.findAll({
+				where: {
+					active: true,
+				},
+				attributes: ['id', 'date'],
+				raw: true,
+				include: [{
+					model: Check_Up,
+					as: 'parent_record',
+					required: true,
+					where: {
+						active: true,
+						doctorId: req.session.doctor.id,
+					},
+					attributes: ['id', 'check_up_type', 'hospitalName', 'doctorId', 'patientId' ],
+					include: [{
+						model: Patient,
+						attributes: ['last_name', 'middle_name', 'first_name']
+					}]
+				}]
+			}).then(opt_list => {
+				console.log(opt_list);
+				Consultation.findAll({
+					attributes: ['id', 'date'],
+					where: {
+						active: true,
+					},
+					raw: true,
+					include: [{
+						model: Check_Up,
+						as: 'parent_record',
+						required: true,
+						where: {
+							active: true,
+							doctorId: req.session.doctor.id,
+						},
+						attributes: ['id', 'check_up_type', 'hospitalName', 'doctorId', 'patientId' ],
+						include: [{
+							model: Patient,
+							attributes: ['last_name', 'middle_name', 'first_name']
+						}]
+					}],
+				}).then(cc_list =>{
+					// console.log(ipt_list.concat(opt_list.concat(cc_list)));
+					console.log(cc_list);
+					allRecords = ipt_list.concat(opt_list.concat(cc_list));
+					var keys = [];
+
+					for(var i = 0; i < allRecords.length; i++){
+						keys.push(allRecords[i]['parent_record.id']);
+					}
+
+					Billing_Item.findAll({
+						raw: true,
+						attributes: [
+							[Sequelize.fn('SUM', Sequelize.col('amount')), 'sumOfColumn'],
+							'checkUpId',
+						],
+						group: ['billing_item.checkUpId'],
+						where: {
+							checkUpId: keys,
+						}
+					}).then(billing_sum => {
+						console.log(billing_sum)
+						res.render('billing/financial-report.html', {
+							reports: allRecords,
+							expenses: billing_sum,
+							session: req.session,
+						});
+					});
+				});
+			});
+		});
+	} else{
+		InPatient_Treatment.findAll({
+			raw: true,
+			where: {
+				active: true
+			},
+			attributes: ['id', 'conf_date'],
+			include: [{
+				model: Check_Up,
+				as: 'parent_record',
+				required: true,
+				where: {
+					active: true,
+				},
+				attributes: ['id', 'check_up_type', 'hospitalName', 'doctorId', 'patientId' ],
+				include: [{
+					model: Billing_Item,
+					as: 'billing_items',
+					required: true,
+				}]
+			}],
+		}).then(ipt_list => {
+			OutPatient_Treatment.findAll({
+				raw: true,
+				where: {
+					active: true,
+				},
+				attributes: ['id', 'date'],
+				include: [{
+					model: Check_Up,
+					as: 'parent_record',
+					required: true,
+					where: {
+						active: true,
+					},
+					attributes: ['id', 'check_up_type', 'hospitalName', 'doctorId', 'patientId' ],
+					include: [{
+						model: Billing_Item,
+						as: 'billing_items',
+						required: true,
+					}]
+				}]
+			}).then(opt_list => {
+				Consultation.findAll({
+					raw: true,
+					attributes: ['id', 'date'],
+					where: {
+						active: true,
+					},
+					include: [{
+						model: Check_Up,
+						as: 'parent_record',
+						required: true,
+						where: {
+							active: true,
+						},
+						attributes: ['id', 'check_up_type', 'hospitalName', 'doctorId', 'patientId' ],
+						include: [{
+							model: Billing_Item,
+							as: 'billing_items',
+							required: true,
+						}]
+					}],
+				}).then(cc_list =>{
+					console.log(ipt_list.concat(opt_list.concat(cc_list)));
+					res.json({
+						meds: ipt_list.concat(opt_list.concat(cc_list)),
+					});
+				});
+			});
+		});
+	}
 });
 
 
